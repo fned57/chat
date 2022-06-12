b7987cb0764feb6a11c72001d6ec9bd7ba4b7d2d
Tuan Tran <fned@gmail.com>
2022-06-12 23:07:51 +0700

    sadsd

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//   });
+// });
 
-http.listen(80, function(){
-  console.log('listening on *:80');
-});
+// http.listen(80, function(){
+//   console.log('listening on *:80');
+// });
