688e3fa63221de725ba69f9bad2e0e2960c6c508
Tuan Tran <fned@gmail.com>
2022-06-12 23:26:54 +0700

    first

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
-// var io = require('socket.io')(http);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-// io.on('connection', function(socket){
-//   socket.on('chat message', function(msg){
-//     io.emit('chat message', msg);
-//   });
-// });
-
-// http.listen(80, function(){
-//   console.log('listening on *:80');
-// });
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+const PORT = process.env.PORT || 80;
+http.listen(PORT, function(){
+  console.log('listening on *:80');
+});
